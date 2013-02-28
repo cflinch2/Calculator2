94b876ad0b18cc902736a2caa4e0c1934a995a2d
TDM0429 <tmille33@ycp.edu>
2013-02-27 21:38:47 -0500

    Slight UI Changes... problems realized

--- UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
+++ UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
@@ -4,19 +4,21 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Button;
 
-import edu.ycp.cs320.calculator.shared.GameList_Test;
-import edu.ycp.cs320.calculator.shared.RocketPadsFrame;
+import edu.ycp.cs320.calculator.shared.GameList;
+import edu.ycp.cs320.calculator.shared.RocketPadsPanel;
 
 
 public class Game_Selection extends Composite {
 	
-	private RocketPadsFrame floor;
+	private RocketPadsPanel floor;
 	private Button selector;
 	private InlineLabel gameSelection;
-	private ListBox gameListBox;
+	private static ListBox gameListBox;
 	
 	
 	public Game_Selection(){
@@ -26,29 +28,38 @@ public class Game_Selection extends Composite {
 		
 		gameSelection = new InlineLabel("Game:");
 		layoutPanel.add(gameSelection);
-		layoutPanel.setWidgetLeftWidth(gameSelection, 10.0, Unit.PX, 80.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(gameSelection, 50.0, Unit.PX, 20.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(gameSelection, 10.0, Unit.PX, 50.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(gameSelection, 10.0, Unit.PX, 20.0, Unit.PX);
 		
 		gameListBox = new ListBox();
 		layoutPanel.add(gameListBox);
-		layoutPanel.setWidgetLeftWidth(gameListBox, 130.0, Unit.PX, 530.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(gameListBox, 50.0, Unit.PX, 20.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(gameListBox, 75.0, Unit.PX, 150.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(gameListBox, 10.0, Unit.PX, 20.0, Unit.PX);
 		
 		selector = new Button();
+		selector.setText("Play!");
+		selector.addClickHandler(new ClickHandler(){
+			@Override
+			public void onClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				
+				
+			}
+		});
 		layoutPanel.add(selector);
-		layoutPanel.setWidgetLeftWidth(selector, 275.0, Unit.PX, 300.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(selector, 40.0, Unit.PX, 35.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(selector, 200.0, Unit.PX, 300.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(selector, 10.0, Unit.PX, 30.0, Unit.PX);
 	}
 	
 	
-	public void setFloor(RocketPadsFrame floor){
+	public void setFloor(RocketPadsPanel floor){
 		this.floor = floor;
 	}
 	
-	public void update(){
+	public static void update(){
 		if (gameListBox.getItemCount() == 0) {
-			GameList_Test[] games = GameList_Test.values();
-			for (GameList_Test g : games) {
+			GameList[] games = GameList.values();
+			for (GameList g : games) {
 				gameListBox.addItem(g.toString());
 			}
 		}
