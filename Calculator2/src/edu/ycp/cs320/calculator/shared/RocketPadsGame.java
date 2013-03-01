ff8b004d1aaa95918390b4db969e290a50cecafc
Colin Bennett <cbennet1@ycp.edu>
2013-03-01 11:13:51 -0500

    RocketPadsGame (Important update) Session (minimal update)

--- Calculator2/src/edu/ycp/cs320/calculator/shared/RocketPadsGame.java
+++ Calculator2/src/edu/ycp/cs320/calculator/shared/RocketPadsGame.java
@@ -3,60 +3,19 @@ package edu.ycp.cs320.calculator.shared;
 public class RocketPadsGame 
 {
 	// Fields
-	int dx, dy; // Direction (+X for positive, 0 for stationary, -X for negative)
-	int currentX, currentY; // Current position
-	int startX, startY; // Original position
-	int winX, winY; // Location of the winning area
-	int length, width; // Dimensions of the playing area
-	boolean slide; // Determines if the player is currently sliding (in which case manual controls are disabled)
+	RocketPadsLocation win_zone; 
+	final int LENGTH = 900, WIDTH = 900;
 	boolean win; // When true, game ends.
 	
 	// Constructor
 	RocketPadsGame()
 	{
-		// These values are arbitrary and should be changed as needed.
-		length = 600;
-		width = 600;
-		dx = 0;
-		dy = 0;
-		currentX = 0;
-		currentY = 0;
-		startX = 0;
-		startY = 0;
-		winX = 300;
-		winY = 300;
-		slide = false;
+		win_zone = new RocketPadsLocation(450,450);
 		win = false;
 	}
 	
-	public void reset_location()
+	public int checkVictory(RocketPadsPlayer player)
 	{
-		currentX = startX;
-		currentY = startY;
+		return player.getLocation().compareLocTo(win_zone);
 	}
-	
-	public boolean getSlide()
-	{
-		return slide;
-	}
-	
-	public void setSlide(boolean s)
-	{
-		slide = s;
-	}
-	
-	public void updatePosition()
-	{
-		currentX += dx;
-		currentY += dy;
-	}
-	
-	public boolean checkVictory()
-	{
-		if(startX == winX && startY == winY)
-			win = true;
-		
-		return win;
-	}
-	
 }
