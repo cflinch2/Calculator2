af83ddeba32edc5c5e0b78947291b08a4baa09ec
Colin Bennett <cbennet1@ycp.edu>
2013-03-01 11:25:30 -0500

    more update for RPG

--- Calculator2/src/edu/ycp/cs320/calculator/shared/RocketPadsGame.java
+++ Calculator2/src/edu/ycp/cs320/calculator/shared/RocketPadsGame.java
@@ -1,17 +1,26 @@
 package edu.ycp.cs320.calculator.shared;
 
+import java.util.ArrayList;
+
 public class RocketPadsGame 
 {
 	// Fields
 	RocketPadsLocation win_zone; 
 	final int LENGTH = 900, WIDTH = 900;
 	boolean win; // When true, game ends.
+	ArrayList<RocketPadsPlayer> players;
 	
 	// Constructor
-	RocketPadsGame()
+	public RocketPadsGame(int num_players)
 	{
-		win_zone = new RocketPadsLocation(450,450);
+		win_zone = new RocketPadsLocation(LENGTH/2,WIDTH/2);
 		win = false;
+		players = new ArrayList<RocketPadsPlayer>();
+		
+		for(int i = 0; i < num_players; i ++)
+		{
+			players.add(new RocketPadsPlayer());
+		}
 	}
 	
 	public int checkVictory(RocketPadsPlayer player)
