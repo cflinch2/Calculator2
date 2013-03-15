28dfd29cf087574a15bd35c743111ba4c208647f
TDM0429 <tmille33@ycp.edu>
2013-03-14 20:52:22 -0400

    Few new Features

---
+++ UserInterface/src/edu/ycp/cs320/calculator/client/Game_Play.java
@@ -0,0 +1,25 @@
+package edu.ycp.cs320.calculator.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.LayoutPanel;
+/*import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Image;*/
+
+public class Game_Play extends Composite {
+	
+	public Game_Play(){
+		
+		LayoutPanel gPlay = new LayoutPanel();
+		initWidget(gPlay);
+		gPlay.setSize("1000px", "750px");
+		
+		
+		
+	}
+}
--- UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
+++ UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
@@ -3,6 +3,7 @@ package edu.ycp.cs320.calculator.client;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -11,17 +12,18 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Image;
 
 import edu.ycp.cs320.calculator.shared.GameList;
-import edu.ycp.cs320.calculator.shared.RocketPadsPanel;
-
-
-
+//import edu.ycp.cs320.calculator.shared.RocketPadsPanel;
 
 public class Game_Selection extends Composite {
 	
-	private RocketPadsPanel floor;
+	//private RocketPadsPanel floor;
 	private Button selector;
 	private InlineLabel gameSelection;
 	private InlineLabel gamePanel;
+	private InlineLabel user;
+	private TextBox uName;
+	private Image rocketpads;
+	private Image image;
 	private static ListBox gameListBox;
 	
 	
@@ -42,9 +44,33 @@ public class Game_Selection extends Composite {
 		layoutPanel.setWidgetLeftWidth(gameListBox, 125.0, Unit.PX, 100.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(gameListBox, 15.0, Unit.PX, 25.0, Unit.PX);
 		
+		user = new InlineLabel("User:");
+		layoutPanel.add(user);
+		layoutPanel.setWidgetLeftWidth(user, 310.0, Unit.PX, 25.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(user, 15.0, Unit.PX, 25.0, Unit.PX);
+		
+		uName = new TextBox();
+		layoutPanel.add(uName);
+		layoutPanel.setWidgetLeftWidth(uName, 350.0, Unit.PX, 100.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(uName, 15.0, Unit.PX, 25.0, Unit.PX);
+		
 		gamePanel = new InlineLabel();
-		layoutPanel.setWidgetLeftWidth(gamePanel, 275.0, Unit.PX, 100.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(gamePanel, 15.0, Unit.PX, 25.0, Unit.PX);
+		layoutPanel.add(gamePanel);
+		layoutPanel.setWidgetLeftWidth(gamePanel, 500.0, Unit.PX, 250.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(gamePanel, 18.0, Unit.PX, 25.0, Unit.PX);
+		
+		rocketpads = new Image();
+		rocketpads.setUrl("WEB-INF/classes/edu/ycp/cs320/calculator/client/rocketpads_board1.jpg");
+		layoutPanel.add(rocketpads);
+		layoutPanel.setWidgetLeftWidth(rocketpads, 125.0, Unit.PX, 500.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(rocketpads, 50.0, Unit.PX, 500.0, Unit.PX);
+		rocketpads.setVisible(false);
+		
+		image = new Image();
+		image.setUrl("WEB-INF/classes/edu/ycp/cs320/calculator/client/old-classic-board-games.jpg");
+		layoutPanel.add(image);
+		layoutPanel.setWidgetLeftWidth(image, 125.0, Unit.PX, 500.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image, 50.0, Unit.PX, 500.0, Unit.PX);
 		
 		selector = new Button();
 		layoutPanel.add(selector);
@@ -53,7 +79,15 @@ public class Game_Selection extends Composite {
 			@Override
 			public void onClick(ClickEvent event) {
 				// TODO Auto-generated method stub
-				gamePanel.setText(gameListBox.getItemText(0));
+				String selGame = getChange();
+				
+				if(selGame == "RocketPads"){
+					rocketpads.setVisible(true);
+					image.setVisible(false);
+					
+				}
+				
+				gamePanel.setText(getUName() + " is Playing " + getChange());
 				
 				
 				
@@ -62,20 +96,26 @@ public class Game_Selection extends Composite {
 		layoutPanel.setWidgetLeftWidth(selector, 250.0, Unit.PX, 100.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(selector, 15.0, Unit.PX, 25.0, Unit.PX);
 		
-		Image image = new Image();
-		image.setUrl("WEB-INF/classes/edu/ycp/cs320/calculator/client/old-classic-board-games.jpg");
-		layoutPanel.add(image);
-		layoutPanel.setWidgetLeftWidth(image, 125.0, Unit.PX, 500.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image, 50.0, Unit.PX, 500.0, Unit.PX);
+		
 		
 		
 		
 	}
 	
-
-	public void setFloor(RocketPadsPanel floor){
-		this.floor = floor;
+	public String getChange(){
+		int sel = gameListBox.getSelectedIndex();
+		String GameChoice = gameListBox.getValue(sel);
+		return GameChoice;
 	}
+	
+	public String getUName(){
+		String name = uName.getText();
+		return name;
+	}
+
+	/*public void setFloor(RocketPadsPanel floor){
+		this.floor = floor;
+	}*/
 	
 	public static void update(){
 		if (gameListBox.getItemCount() == 0) {
