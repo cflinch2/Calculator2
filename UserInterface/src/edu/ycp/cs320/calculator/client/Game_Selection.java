3665d071be8021dfdbfd546475657627230587bd
TDM0429 <tmille33@ycp.edu>
2013-03-15 11:34:54 -0400

    Yeah

--- UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
+++ UserInterface/src/edu/ycp/cs320/calculator/client/Game_Selection.java
@@ -36,71 +36,58 @@ public class Game_Selection extends Composite {
 			
 		gameSelection = new InlineLabel("Game:");
 		layoutPanel.add(gameSelection);
-		layoutPanel.setWidgetLeftWidth(gameSelection, 15.0, Unit.PX, 100.0, Unit.PX );
+		layoutPanel.setWidgetLeftWidth(gameSelection, 15.0, Unit.PX, 50.0, Unit.PX );
 		layoutPanel.setWidgetTopHeight(gameSelection, 15.0, Unit.PX, 25.0, Unit.PX);
 		
 		gameListBox = new ListBox();
 		layoutPanel.add(gameListBox);
-		layoutPanel.setWidgetLeftWidth(gameListBox, 125.0, Unit.PX, 100.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(gameListBox, 75.0, Unit.PX, 100.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(gameListBox, 15.0, Unit.PX, 25.0, Unit.PX);
 		
 		user = new InlineLabel("User:");
 		layoutPanel.add(user);
-		layoutPanel.setWidgetLeftWidth(user, 310.0, Unit.PX, 25.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(user, 185.0, Unit.PX, 50.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(user, 15.0, Unit.PX, 25.0, Unit.PX);
 		
 		uName = new TextBox();
 		layoutPanel.add(uName);
-		layoutPanel.setWidgetLeftWidth(uName, 350.0, Unit.PX, 100.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(uName, 245.0, Unit.PX, 100.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(uName, 15.0, Unit.PX, 25.0, Unit.PX);
 		
-		gamePanel = new InlineLabel();
-		layoutPanel.add(gamePanel);
-		layoutPanel.setWidgetLeftWidth(gamePanel, 500.0, Unit.PX, 250.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(gamePanel, 18.0, Unit.PX, 25.0, Unit.PX);
-		
-		rocketpads = new Image();
-		rocketpads.setUrl("WEB-INF/classes/edu/ycp/cs320/calculator/client/rocketpads_board1.jpg");
-		layoutPanel.add(rocketpads);
-		layoutPanel.setWidgetLeftWidth(rocketpads, 125.0, Unit.PX, 500.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(rocketpads, 50.0, Unit.PX, 500.0, Unit.PX);
-		rocketpads.setVisible(false);
-		
-		image = new Image();
-		image.setUrl("userinterface/old-classic-board-games.jpg");
-		layoutPanel.add(image);
-		layoutPanel.setWidgetLeftWidth(image, 125.0, Unit.PX, 500.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(image, 50.0, Unit.PX, 500.0, Unit.PX);
-		
-		selector = new Button();
+				selector = new Button();
 		layoutPanel.add(selector);
 		selector.setText("Play!");
 		selector.addClickHandler(new ClickHandler(){
 			@Override
 			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
+				
 				String selGame = getChange();
 				
 				if(selGame == "RocketPads"){
 					rocketpads.setVisible(true);
-					image.setVisible(false);
-					//Game_play.Game_play();	
-					
+														
 				}
 				
 				gamePanel.setText(getUName() + " is Playing " + getChange());
-				
-				
-				
+								
 			}
 		});
-		layoutPanel.setWidgetLeftWidth(selector, 250.0, Unit.PX, 100.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(selector, 355.0, Unit.PX, 100.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(selector, 15.0, Unit.PX, 25.0, Unit.PX);
 		
+		gamePanel = new InlineLabel();
+		layoutPanel.add(gamePanel);
+		layoutPanel.setWidgetLeftWidth(gamePanel, 410.0, Unit.PX, 50.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(gamePanel, 15.0, Unit.PX, 25.0, Unit.PX);
 		
 		
 		
-		
+		image = new Image();
+		image.setUrl("userinterface/lobby_screen.png");
+		layoutPanel.add(image);
+		layoutPanel.setWidgetLeftWidth(image, 125.0, Unit.PX, 500.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(image, 50.0, Unit.PX, 500.0, Unit.PX);
+				
 	}
 	
 	public String getChange(){
