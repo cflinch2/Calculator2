1fa80c0cc48b7e186922d8fc920f56236a23a5a2
TDM0429 <tmille33@ycp.edu>
2013-05-03 11:52:33 -0400

    ...

--- Calculator2/JUnit/edu/ycp/cs320/calculator/shared/RocketPadsGameTest.java
+++ Calculator2/JUnit/edu/ycp/cs320/calculator/shared/RocketPadsGameTest.java
@@ -3,5 +3,17 @@ package edu.ycp.cs320.calculator.shared;
 import junit.framework.TestCase;
 
 public class RocketPadsGameTest extends TestCase{
+	RocketPadsGame testGame;
+		
+	public void testWin(){
+		testGame.setWin(false);
+		assertEquals(false, testGame.checkWin());
+		
+		testGame.setWin(true);
+		assertEquals(true, testGame.checkWin());
+	}
 	
+	public void testGetPlayers(){
+		assertEquals(1, testGame.get_num_players());
+	}
 }
