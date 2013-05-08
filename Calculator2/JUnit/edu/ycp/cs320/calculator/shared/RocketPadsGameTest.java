d30c90a2a93f79f8974427bc43623a2791eab52a
Colin Bennett <cbennet1@ycp.edu>
2013-05-08 11:05:07 -0400

    RocketPadsGame JUnit Test

--- Calculator2/JUnit/edu/ycp/cs320/calculator/shared/RocketPadsGameTest.java
+++ Calculator2/JUnit/edu/ycp/cs320/calculator/shared/RocketPadsGameTest.java
@@ -2,7 +2,7 @@ package edu.ycp.cs320.calculator.shared;
 
 import junit.framework.TestCase;
 
-public class RocketPadsGameTest extends TestCase{
+public class RocketPadsGameTest extends TestCase {
 	RocketPadsGame testGame;
 	private int[][] tutorial = {
 		{6,3,3,3,3,3,3,3,3,3,3,2},
@@ -18,12 +18,11 @@ public class RocketPadsGameTest extends TestCase{
 		{2,5,5,5,5,5,5,5,5,5,5,2},
 		{3,3,3,3,3,3,3,3,3,3,3,7}};
 		
-	public void setUp(){
+	public void setUp() {
 		testGame = new RocketPadsGame(RocketPadsCreateBoardDataFromArray.createFromArray(tutorial, BoardList.Tutorial));
-		
 	}
 	
-	public void testWin(){
+	public void testWin() {
 		testGame.setWin(false);
 		assertEquals(false, testGame.checkWin());
 		
@@ -31,13 +30,11 @@ public class RocketPadsGameTest extends TestCase{
 		assertEquals(true, testGame.checkWin());
 	}
 	
-	public void testGetPlayers(){
+	public void testGetPlayers() {
 		assertEquals(1, testGame.get_num_players());
 	}
 	
-	public void testGetPad(){
+	public void testGetPad() {
 		assertEquals(RocketPadsDirection.STOP, testGame.getPad(1,1));		
 	}
-	
-
 }
